894db44793ab68c607323520a53a45ccad9132cb
Leodgar2010 <leodgar2010@yandex.ru>
2023-09-03 21:53:23 +0300

    Fifth homework

---
+++ myproject/app1/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+
+from app1.views import add_product, add_client
+
+urlpatterns = [
+    path ('product/',add_product, name ='product'),
+    path ('client/',add_client, name ='client')
+]
